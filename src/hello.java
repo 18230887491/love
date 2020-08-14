998df1bcd9940a822e61905d20054316a075d3b0
张斌 <522864514@qq.com>
2020-08-14 19:09:10 +0800

    李康第一次提交

---
+++ src/hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther:http://www.bjsxt.com
+ * @Date:2020/8/14
+ * @Description:PACKAGE_NAME
+ * @version：1.0
+ */
+public class hello {
+    public  void fun(){
+        System.out.println("哈哈");
+    }
+}
