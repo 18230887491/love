7a96b1d1639d9ed29b56c261761157de56f28dd0
张斌 <522864514@qq.com>
2020-08-14 19:21:53 +0800

    李康第一次提交mygit01

---
+++ mygit01/src/com/gec/dao/Hello.java
@@ -0,0 +1,13 @@
+package com.gec.dao;
+
+/**
+ * @Auther:http://www.bjsxt.com
+ * @Date:2020/8/14
+ * @Description:com.gec.dao
+ * @version：1.0
+ */
+public interface Hello {
+    //李康加入一个方法
+    public  void fun();
+
+}
